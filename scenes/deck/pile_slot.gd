f3248eec751a38631f671d94ebea867a65e93fc2
s3r0s4pi3ns <s3r0s4pi3ns@protonmail.com>
2023-08-29 17:21:54 +0100

    refactor pile slot to manage better the cards played on the target pile

--- scenes/deck/pile_slot.gd
+++ scenes/deck/pile_slot.gd
@@ -15,6 +15,8 @@ func _ready():
 	
 	if not is_in_group(GROUP_NAME):
 		add_to_group(GROUP_NAME)
+		
+	card_dropped.connect(on_card_dropped)
 	
 	
 func _can_drop_data(_at_position, data):
@@ -24,33 +26,14 @@ func _can_drop_data(_at_position, data):
 func _drop_data(at_position, data):
 	var dropped_card = data.playing_card as PlayingCard
 	
-	cards_in_pile.append(dropped_card)
-	
 	if current_suit.is_empty() and not dropped_card.is_poison:
 		current_suit = dropped_card.suit
 		self_modulate.a = 0.0
 	
-	current_points += dropped_card.current_value
-	
-	if current_points >= 13:
-		pile_collected.emit(data.player, self)
-		cards_in_pile.clear()
-		current_suit = ""
-		
-	var card_pile_texture = TextureRect.new()
-	card_pile_texture.name = dropped_card.suit.capitalize() + dropped_card.symbol_value.capitalize()
-	card_pile_texture.texture = dropped_card.symbol_texture.texture
-	card_pile_texture.expand_mode = TextureRect.EXPAND_KEEP_SIZE
-	card_pile_texture.size = dropped_card.symbol_texture.get_rect().size
-	card_pile_texture.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	
-	if dropped_card.is_poison:
-		card_pile_texture.position.y += 21
-		
-	add_child(card_pile_texture)
-	
+	add_points_to_pile(dropped_card.current_value)
+	add_card_to_pile(dropped_card)
+
 	data.player.cards_in_hand.erase(dropped_card)
-	
 	card_dropped.emit(data.player, dropped_card, self)
 	
 
@@ -64,5 +47,37 @@ func suit_is_not_active(suit: String) -> bool:
 	var suits = get_tree().get_nodes_in_group(GROUP_NAME).map(func(pile: PileSlot): return pile.current_suit)
 	
 	return suit not in suits
+
+
+func add_card_to_pile(card: PlayingCard) -> void:
+	var card_pile_texture = TextureRect.new()
+	card_pile_texture.name = card.suit.capitalize() + card.symbol_value.capitalize()
+	card_pile_texture.texture = card.symbol_texture.texture
+	card_pile_texture.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+	card_pile_texture.size = card.symbol_texture.get_rect().size
+	card_pile_texture.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	if card.is_poison:
+		card_pile_texture.position.y += 21
+		
+	add_child(card_pile_texture)
+
+	cards_in_pile.append(card)
+
+
+func add_points_to_pile(value: float) -> void:
+	current_points += value
 	
 	
+	
+func on_card_dropped(player, _card, _pile):
+	if current_points >= 13:
+		pile_collected.emit(player, duplicate())
+		cards_in_pile.clear()
+		current_suit = ""
+		current_points = 0.0
+		self_modulate.a = 0.2
+		
+		for child in get_children():
+			child.queue_free()
+		
